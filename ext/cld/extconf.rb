fe35dec1e51dcebca00250a691afed3954b34193
Luka Dornhecker <luka.dornhecker@toptranslation.com>
2018-11-19 10:51:03 +0100

    fix: add -Wno-narrowing to compiler flags so it compiles on modern systems

--- ext/cld/extconf.rb
+++ ext/cld/extconf.rb
@@ -1,4 +1,7 @@
 #require 'mkmf'
 #create_makefile('cld/cld')
 
+ENV['CFLAGS'] = ENV['CFLAGS'].to_s + ' -Wno-narrowing'
+ENV['CXXFLAGS'] = ENV['CXXFLAGS'].to_s + ' -Wno-narrowing'
+
 system "./configure --prefix=#{Dir.pwd}" unless File.exists?('Makefile')
