46d834f894aca9525b533600858e82017bc9fd27
Michael Grosser <michael@grosser.it>
2021-07-15 07:30:14 -0700

    Revert "Merge pull request #17 from lizthegrey/master"
    This reverts commit 80bb892bef9a96769629acfdfeadd0662b8917bb, reversing
    changes made to bd4e4a19691c642c52fba8589dada50e3f1365e7.
    
    broke regular builds see https://github.com/jtoy/cld/pull/17#issuecomment-880547344

--- ext/cld/base/build_config.h
+++ ext/cld/base/build_config.h
@@ -89,11 +89,6 @@
 #define ARCH_CPU_ARMEL 1
 #define ARCH_CPU_32_BITS 1
 #define WCHAR_T_IS_UNSIGNED 1
-#elif defined(__aarch64__)
-#define ARCH_CPU_ARM_FAMILY 1
-#define ARCH_CPU_ARM64 1
-#define ARCH_CPU_64_BITS 1
-#define ARCH_CPU_LITTLE_ENDIAN 1
 #else
 #error Please add support for your architecture in build/build_config.h
 #endif
